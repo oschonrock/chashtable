b260be2ac232f848ac3f5c385dfe4738a0d14af2
Oliver Schönrock <oliver@openbrackets.net>
2021-01-18 22:21:44 +0000

    more detailed insert delete tests
    looking carefully at slot distribution and slot count

--- tests/test_hashtable.c
+++ tests/test_hashtable.c
@@ -1,5 +1,7 @@
 #include "hashtable.h"
 #include "unity.h"
+#include <string.h>
+#include <stdlib.h>
 
 static HashTable *ht;
 
@@ -7,27 +9,45 @@ void setUp(void) { ht = ht_create(4); }
 
 void tearDown(void) { ht_free(ht); }
 
-void test_insert_count_check_delete_count(void) {
+void test_insert_delete(void) {
   TEST_ASSERT_EQUAL(0, ht->itemcount);
 
-  ht_insert(ht, "key", 10);
-  // ht_print(ht);
-
+  ht_insert(ht, "aaa", 10); // @2
   TEST_ASSERT_EQUAL(1, ht->itemcount);
   TEST_ASSERT_EQUAL(1, ht->scount);
 
-  HashTableItem *item = ht_search(ht, "key");
+  HashTableItem *item = ht_search(ht, "aaa");
   TEST_ASSERT_NOT_NULL(item);
   TEST_ASSERT_EQUAL(10, item->value);
 
-  ht_delete(ht, "key");
+  ht_insert(ht, "mmm", 10); // @2
+  
+  ht_insert(ht, "bbb", 10); // @1 
+  ht_insert(ht, "jjj", 10); // @1
+  ht_insert(ht, "rrr", 10); // @1
+
+  ht_insert(ht, "lll", 10); // @3
+  TEST_ASSERT_EQUAL(6, ht->itemcount);
+  TEST_ASSERT_EQUAL(3, ht->scount);
+
+  ht_delete(ht, "bbb");
+  TEST_ASSERT_EQUAL(3, ht->scount);
+  ht_delete(ht, "jjj");
+  TEST_ASSERT_EQUAL(3, ht->scount);
+  ht_delete(ht, "rrr");
+  TEST_ASSERT_EQUAL(2, ht->scount);
+  ht_delete(ht, "mmm");
+  TEST_ASSERT_EQUAL(2, ht->scount);
+  ht_delete(ht, "aaa");
+  TEST_ASSERT_EQUAL(1, ht->scount);
+  ht_delete(ht, "lll");
   TEST_ASSERT_EQUAL(0, ht->itemcount);
   TEST_ASSERT_EQUAL(0, ht->scount);
 
-  HashTableItem *item2 = ht_search(ht, "key");
+  HashTableItem *item2 = ht_search(ht, "aaa");
   TEST_ASSERT_NULL(item2);
 }
-void test_inc_count_check(void) {
+void test_inc(void) {
   ht_inc(ht, "aaa");
   TEST_ASSERT_EQUAL(1, ht->itemcount);
   TEST_ASSERT_EQUAL(1, ht->scount);
@@ -42,24 +62,50 @@ void test_inc_count_check(void) {
   TEST_ASSERT_EQUAL(3, ht->scount);
   TEST_ASSERT_EQUAL(4, ht->size);
 
-  ht_inc(ht, "aaa");
-
   HashTableItem *a = ht_search(ht, "aaa");
   HashTableItem *b = ht_search(ht, "bbb");
   HashTableItem *c = ht_search(ht, "ccc");
-  TEST_ASSERT_NOT_NULL(a);
-  TEST_ASSERT_EQUAL(2, a->value);
 
+  ht_inc(ht, "aaa");
+  ht_inc(ht, "aaa");
+  TEST_ASSERT_NOT_NULL(a);
+  TEST_ASSERT_EQUAL(3, a->value);
+
+  ht_inc(ht, "bbb");
   TEST_ASSERT_NOT_NULL(b);
-  TEST_ASSERT_EQUAL(1, b->value);
+  TEST_ASSERT_EQUAL(2, b->value);
 
   TEST_ASSERT_NOT_NULL(c);
   TEST_ASSERT_EQUAL(1, c->value);
 }
 
+void test_grow() {
+  ht_inc(ht, "aaa");
+  ht_inc(ht, "bbb");
+  ht_inc(ht, "ccc");
+  ht_inc(ht, "ddd"); // grow on this one
+  TEST_ASSERT_EQUAL(4, ht->itemcount);
+  TEST_ASSERT_EQUAL(4, ht->scount);
+  TEST_ASSERT_EQUAL(8, ht->size);
+}
+
+void test_flat_view() {
+  ht_inc(ht, "aaa");
+  ht_inc(ht, "bbb");
+  ht_inc(ht, "ccc2");
+  HashTableItem** view = ht_create_flat_view(ht);
+  
+  TEST_ASSERT_EQUAL(0, strcmp("bbb", view[0]->key));
+  TEST_ASSERT_EQUAL(0, strcmp("aaa", view[1]->key));
+  TEST_ASSERT_EQUAL(0, strcmp("ccc2", view[2]->key));
+  free(view);
+}
+
 int main() {
   UNITY_BEGIN();
-  RUN_TEST(test_insert_count_check_delete_count);
+  RUN_TEST(test_insert_delete);
   RUN_TEST(test_inc);
+  RUN_TEST(test_grow);
+  RUN_TEST(test_flat_view);
   return UNITY_END();
 }
