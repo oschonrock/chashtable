880d90d27cef5c6c187b463f74dbb0ec62dc4a45
Oliver Schönrock <oliver@openbrackets.net>
2021-01-21 09:21:29 +0000

    more strcuture

--- apps/topwords.c
+++ apps/topwords.c
@@ -19,33 +19,6 @@ static int cmp_ht_items(const void* a, const void* b) {
   return a_val < b_val ? 1 : -1;
 }
 
-static void parse_and_map(FILE* fp, HashTable* table) {
-#define BUFSIZE 1024
-#define WORDSIZE 50
-  char  buf[BUFSIZE];
-  char  word[WORDSIZE];
-  char* word_ptr = word;
-  while (!feof(fp) && !ferror(fp)) {
-    size_t bread  = fread(buf, 1, BUFSIZE, fp);
-    char*  bufptr = buf;
-    while ((bufptr < buf + bread)) {
-      char c = *bufptr;
-      if (ht_is_alpha(c)) {
-        *word_ptr++ = ht_tolower(c);
-        if (word_ptr - word == WORDSIZE - 1) { // -1 for NULL terminator
-          fputs("word too long. terminating\n", stderr);
-          exit(EXIT_FAILURE);
-        }
-      } else if (word_ptr > word) {
-        *word_ptr++ = '\0';  // terminate word
-        ht_inc(table, word); // record (takes a copy)
-        word_ptr = word;     // restart new word
-      }
-      ++bufptr; // next char from buf
-    }
-  }
-}
-
 bool parseul(const char* str, size_t* val) {
   char* end; // NOLINT
   errno = 0;
@@ -74,10 +47,66 @@ static void print_elapsed(timespec start, timespec end, char* label) {
   printf("%s: %ld.%09lds\n", label, e.tv_sec, e.tv_nsec);
 }
 
-static void rand_ht_bench() {
+static void parse_and_map(FILE* fp, size_t limit) {
+  HashTable* ht = ht_create(32 * 1024);
+
+  timespec start, stop;
+  clock_gettime(CLOCK_MONOTONIC, &start);
+  
+#define BUFSIZE 1024
+#define WORDSIZE 50
+  char  buf[BUFSIZE];
+  char  word[WORDSIZE];
+  char* word_ptr = word;
+  while (!feof(fp) && !ferror(fp)) {
+    size_t bread  = fread(buf, 1, BUFSIZE, fp);
+    char*  bufptr = buf;
+    while ((bufptr < buf + bread)) {
+      char c = *bufptr;
+      if (ht_is_alpha(c)) {
+        *word_ptr++ = ht_tolower(c);
+        if (word_ptr - word == WORDSIZE - 1) { // -1 for NULL terminator
+          fputs("word too long. terminating\n", stderr);
+          exit(EXIT_FAILURE);
+        }
+      } else if (word_ptr > word) {
+        *word_ptr++ = '\0';  // terminate word
+        ht_inc(ht, word); // record (takes a copy)
+        word_ptr = word;     // restart new word
+      }
+      ++bufptr; // next char from buf
+    }
+  }
+  
+  clock_gettime(CLOCK_MONOTONIC, &stop);
+
+  // build a flat view of the hashtable items
+  HashTableItem** view = ht_create_flat_view(ht);
+
+  // now sort that view and print the top 10
+  qsort(view, ht->itemcount, sizeof(HashTableItem*), cmp_ht_items);
+  size_t wordcnt = 0;
+  for (size_t i = 0; i < ht->itemcount; i++) wordcnt += view[i]->value;
+
+  printf("\n%s\n----------------------------\n", "file wordcounts");
+  printf("%-17s %'10zu\n", "Word count", wordcnt);
+  printf("%-17s %'10zu\n", "Unique count", ht->itemcount);
+  printf("%-17s %'10zu\n", "Slot count", ht->size);
+  print_elapsed(start, stop, "read + parse + ht_inc()");
+
+  printf("\nTop %zu\n----------------------------\n", limit);
+  for (size_t i = 0; i < minul(limit, ht->itemcount); i++)
+    printf("%-13s %'6d %6.2f%%\n", view[i]->key, view[i]->value,
+           100.0 * view[i]->value / wordcnt);
+
+  free(view);
+  ht_free(ht);
+
+}
+
+static void rand_ht_bench(size_t limit) {
   srand(1); // fixed seed
-  size_t limit = 10;
-  // string demo 1: pointers and strings dynamically allocated on heap
+
   const size_t str_count      = 1000000;
   const size_t str_min_length = 1;
   const size_t str_max_length = 3;
@@ -130,9 +159,6 @@ static void rand_ht_bench() {
 }
 
 int main(int argc, char** argv) {
-  setlocale(LC_NUMERIC, ""); // for thousands separator
-  rand_ht_bench();
-
   char usage[50];
   snprintf(usage, 50, "Usage: %s filename [limit]\n", argv[0]);
   if (argc < 2) {
@@ -153,33 +179,10 @@ int main(int argc, char** argv) {
       exit(EXIT_FAILURE);
     }
   }
-  HashTable* ht = ht_create(32 * 1024);
+  setlocale(LC_NUMERIC, ""); // for thousands separator
 
-  timespec start, stop;
-  clock_gettime(CLOCK_MONOTONIC, &start);
-  parse_and_map(fp, ht);
-  clock_gettime(CLOCK_MONOTONIC, &stop);
+  rand_ht_bench(limit);
+  parse_and_map(fp, limit);
+  
   fclose(fp);
-
-  // build a flat view of the hashtable items
-  HashTableItem** view = ht_create_flat_view(ht);
-
-  // now sort that view and print the top 10
-  qsort(view, ht->itemcount, sizeof(HashTableItem*), cmp_ht_items);
-  size_t wordcnt = 0;
-  for (size_t i = 0; i < ht->itemcount; i++) wordcnt += view[i]->value;
-
-  printf("\n%s\n----------------------------\n", argv[1]);
-  printf("%-17s %'10zu\n", "Word count", wordcnt);
-  printf("%-17s %'10zu\n", "Unique count", ht->itemcount);
-  printf("%-17s %'10zu\n", "Slot count", ht->size);
-  print_elapsed(start, stop, "read + parse + ht_inc()");
-
-  printf("\nTop %zu\n----------------------------\n", limit);
-  for (size_t i = 0; i < minul(limit, ht->itemcount); i++)
-    printf("%-13s %'6d %6.2f%%\n", view[i]->key, view[i]->value,
-           100.0 * view[i]->value / wordcnt);
-
-  free(view);
-  ht_free(ht);
 }
