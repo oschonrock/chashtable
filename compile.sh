a731b1fe446d66e57f3010251e216b759b1c798d
Oliver Schönrock <oliver@openbrackets.net>
2023-11-24 05:37:56 +0000

    tidy up

--- compile.sh
+++ compile.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 
-dir="$(dirname $(realpath $0))"
+# switch wd to root of repo
+cd "$(dirname $(realpath $0))"
 
-cd "$dir" && \
-    cmake -DCMAKE_BUILD_TYPE=Release -B build -S . && \
+cmake -DCMAKE_BUILD_TYPE=Release -B build -S . && \
     cmake --build build -- -j8 && \
     ./build/tests/test_hashtable && \
     ./build/bin/topwords ./data/shakespeare.txt 10
