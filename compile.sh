65af074442e16df0a6e3262f2da5fc8baec22536
Oliver Schönrock <oliver@openbrackets.net>
2023-11-24 05:32:11 +0000

    remove local directory reference and format

--- compile.sh
+++ compile.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/bash
 
-cd ~/c/toby/cproj && cmake -DCMAKE_BUILD_TYPE=Release -B build -S . && cmake --build build -- -j8 && ./build/tests/test_hashtable && ./build/bin/topwords ./data/shakespeare.txt 10
+dir="$(dirname $(realpath $0))"
+
+cd "$dir" && \
+    cmake -DCMAKE_BUILD_TYPE=Release -B build -S . && \
+    cmake --build build -- -j8 && \
+    ./build/tests/test_hashtable && \
+    ./build/bin/topwords ./data/shakespeare.txt 10
